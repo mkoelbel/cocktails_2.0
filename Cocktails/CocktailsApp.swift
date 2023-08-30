1bc8a782a39033f24fca8eea7b17edcac54214ea
Maria Koelbel <mariackoelbel@gmail.com>
2023-08-29 22:27:14 -0400

    Initial Commit

---
+++ Cocktails/CocktailsApp.swift
@@ -0,0 +1,17 @@
+//
+//  CocktailsApp.swift
+//  Cocktails
+//
+//  Created by Maria Koelbel on 8/29/23.
+//
+
+import SwiftUI
+
+@main
+struct CocktailsApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
